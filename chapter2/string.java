f5956e5a8730a4f22301763007845331ecd9a386
piyush rathore <piysuh260305@gmail.com>
2024-10-10 01:00:11 +0530

    make little changes in string and add loops quesetion

--- chapter2/string.java
+++ chapter2/string.java
@@ -1,27 +1,27 @@
 public class string {
-    //// 1 question
-    // public static void main(String[] args){
-    // String name= "PIYUSH RATHORE";
-    // System.out.println(name.toLowerCase());
-    // }
+    // 1 question
+    public static void main(String[] args){
+    String name= "PIYUSH RATHORE";
+    System.out.println(name.toLowerCase());
+    }
 
-    //// 2nd question
-    // public static void main(String[] args) {
-    // String something_with_space="a b c d";
-    // System.out.println(something_with_space.replace(" ", "_"));
-    // }
+    // 2nd question
+    public static void main(String[] args) {
+    String something_with_space="a b c d";
+    System.out.println(something_with_space.replace(" ", "_"));
+    }
 
-    //// 3rd question
-    // public static void main(String[] args) {
-    // String name ="piyush rathore";
-    // boolean contains_double_spaces=name.contains(" ");
-    // System.out.println(contains_double_spaces);
-    // }
+    // 3rd question
+    public static void main(String[] args) {
+    String name ="piyush rathore";
+    boolean contains_double_spaces=name.contains(" ");
+    System.out.println(contains_double_spaces);
+    }
 
-    //// 4th question
-    // public static void main(String[] args) {
-    //     String letter = " Dear Harry , This Java Course is nice .Thanks";
-    //     System.out.printf("%s", letter);
-    // }
+    // 4th question
+    public static void main(String[] args) {
+        String letter = " Dear Harry , This Java Course is nice .Thanks";
+        System.out.printf("%s", letter);
+    }
 
 }
