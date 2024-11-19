4061feded92815a8da88d5bf48e38619d437f2fa
Piyushrathoree <meonpath008@gmail.com>
2024-11-19 22:54:51 +0530

    code added

---
+++ variable.java
@@ -0,0 +1,15 @@
+// variables are basically alphabets which hold some values
+
+public class variable {
+    public static void main (String[] args){
+        int number = 10; // integer variable
+        double decimal = 3.14; // double variable
+        char character = 'A'; // character variable
+        boolean booleanValue = true; // boolean variable
+        System.out.println( number );
+        System.out.println(decimal);
+        System.out.println(character);
+        System.out.println(booleanValue );
+
+    }
+}
