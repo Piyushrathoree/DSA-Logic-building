418bc21a36bd59a232c6067a8fe7a9892fb9ac7b
Piyushrathoree <meonpath008@gmail.com>
2024-12-05 19:13:37 +0530

    learned about bitwise operator and binary conversions

---
+++ Cpp/bitwiseOp/bitwise01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+    // bitwise and , bitwise or and bitwise xor 
+    cout << ( 6 & 10 )<<endl;
+    cout << ( 6 | 10) <<endl;
+    cout << ( 6 ^ 10) <<endl;
+
+    //bitwise left shift and bitwise right shift 
+    cout << (10 << 2) <<endl;
+    cout << (10 >> 1) <<endl;
+    return 0;
+}
