8c659ed57e3411a566d52038688b8af25048a1d1
Piyushrathoree <meonpath008@gmail.com>
2025-01-06 03:05:46 +0530

    code file added for delegation constructor

---
+++ Cpp/OOP/delegationConstructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class One{
+    string a;
+    int b;
+    public:
+    //delegation constructor - for setting default values which is not given by user 
+        One(string a): One(a , 1 ){}
+
+    One(string a, int b){
+        a = a;
+        b = b;
+        cout << "a: " << a << " b: " << b << endl;
+    }
+};
+int main(){
+    One obj("Hello");
+    return 0;
+}
