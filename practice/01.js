fd6efe7824fad2a9f7d478c324a7696808aae1be
Piyushrathoree <meonpath008@gmail.com>
2025-01-22 13:48:40 +0530

    code file added js problem solving

---
+++ practice/01.js
@@ -0,0 +1,17 @@
+//find the largest number of array 
+
+let arr = [4,7,2,1,7];
+const largestNum= Math.max(...arr)
+
+
+//reverse the array
+const rev_array= arr.reverse()
+console.log(rev_array);
+
+
+//smallest num
+const smallest=Math.min(...rev_array)
+console.log(smallest);
+
+
+//
