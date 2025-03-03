bd42c57bd8f282c18eb63da2a1fbe00c29cc4185
Piyushrathoree <meonpath008@gmail.com>
2025-03-04 01:51:03 +0530

    add string reversal and variable swapping examples in 05.js

--- practice/05.js
+++ practice/05.js
@@ -1,8 +1,41 @@
 // 29-01-25
 
-//for reversing a string 
-function reverseString(str){
+//for reversing a string
+function reverseString(str) {
     console.log(str.split("").reverse().join(""));
-    
 }
-reverseString("hello")
+reverseString("hello");
+
+// swapping values of variables
+//1st method
+
+let a = 10;
+let b = 20;
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+
+//2nd method
+let p = 50;
+let q = 60;
+
+p = p + q;
+q = p - q;
+p = p - q;
+
+console.log(p);
+console.log(q);
+
+// 3rd method
+
+let x = 100;
+let y = 101;
+
+[x, y] = [y, x];
+console.log(x);
+console.log(y);
